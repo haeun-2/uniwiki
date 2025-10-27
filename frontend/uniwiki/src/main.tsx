92db2675d7b84712d386ca0678feaa97ad4120f5
이지오 <happygeo2006@naver.com>
2025-10-27 14:42:31 +0900

    chore: login/signup routes

--- frontend/uniwiki/src/main.tsx
+++ frontend/uniwiki/src/main.tsx
@@ -4,16 +4,20 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import RootLayout from './layout/RootLayout'
 import UniWikiMainPage from './pages/UniWikiMainPage'
+import LoginPage from './pages/LoginPage'   
+import SignupPage from './pages/SignupPage'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootLayout />,
     children: [
       { index: true, element: <UniWikiMainPage /> },
+      { path: 'login', element: <LoginPage /> },   
+      { path: 'signup', element: <SignupPage />},
     ],
   },
-]);
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
