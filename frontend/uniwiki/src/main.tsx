006a2793b514735b7ba796af486014e82d61f150
박상훈 <psh4060144@gmail.com>
2025-10-27 13:32:55 +0900

    layout 및 main.tsx 구조 변경

--- frontend/uniwiki/src/main.tsx
+++ frontend/uniwiki/src/main.tsx
@@ -2,11 +2,18 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from './layout/RootLayout'
 import UniWikiMainPage from './pages/UniWikiMainPage'
 
 const router = createBrowserRouter([
-  { path: '/', element: <UniWikiMainPage /> },
-])
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <UniWikiMainPage /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
