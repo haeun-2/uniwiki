64ec85975091e4b8425488efdefcedf498c69c4a
김소은 <prgrm0505@gmail.com>
2025-11-05 14:19:40 +0900

    [BE] fix: 대학-지역 LazyInitializationException 해결

--- backend/uniwiki/src/main/java/com/kiwi/uniwiki/domain/university/repository/UniversityRepository.java
+++ backend/uniwiki/src/main/java/com/kiwi/uniwiki/domain/university/repository/UniversityRepository.java
@@ -1,8 +1,9 @@
 package com.kiwi.uniwiki.domain.university.repository;
 
 import com.kiwi.uniwiki.domain.university.entity.University;
-import com.kiwi.uniwiki.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,16 +18,15 @@ public interface UniversityRepository extends JpaRepository<University, Short> {
     Optional<University> findByEmailDomain(String emailDomain);
 
     /**
-     * 도메인 존재 여부 확인
-     */
-    boolean existsByEmailDomain(String emailDomain);
-
-    /**
      * 특정 지역 대학 목록 조회
      */
     List<University> findAllByRegionId(Short regionId);
 
-
-
-
+    @Query("""
+            SELECT u
+            FROM University u
+            JOIN FETCH u.region
+            WHERE u.id = :id
+           """)
+    Optional<University> findByIdWithRegion(@Param("id") Short id);
 }
